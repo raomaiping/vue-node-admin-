62004b9949220154b65d0bca11ac9e73d28c7926
raomaiping <2582395486@qq.com>
2020-01-16 17:02:26 +0800

    使用全球公认头像gravatar

--- node-app/routes/api/users.js
+++ node-app/routes/api/users.js
@@ -8,6 +8,9 @@ const bcrypt = require("bcryptjs");
 //引入User数据模型
 const  User = require("../../models/User");
 
+//引入全球公认头像模块
+const gravatar = require('gravatar');
+
 // $route  GET  api/users/test
 // @desc   返回的请求的json数据
 // @access public
@@ -27,10 +30,12 @@ router.post("/register",(req,res) => {
             if(user){
                 return res.status(400).json({email:"邮箱已被注册"});
             }else{
+                const avatar = gravatar.url(req.body.email, {s: '200', r: 'pg', d: 'mm'});
+
                 const newUser = new User({
                     name:req.body.name,
                     email:req.body.email,
-                    // avatar,
+                    avatar,
                     password:req.body.password
                 })
 
