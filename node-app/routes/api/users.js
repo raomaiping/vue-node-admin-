713208647e227e03bc4f897baee4576eb58fb868
raomaiping <2582395486@qq.com>
2020-01-16 18:50:43 +0800

    增加身份字段

--- node-app/routes/api/users.js
+++ node-app/routes/api/users.js
@@ -20,9 +20,9 @@ const passport = require("passport");
 // $route  GET  api/users/test
 // @desc   返回的请求的json数据
 // @access public
-router.get("/test",(req,res) => {
-    res.json({msg:"login works"})
-})
+// router.get("/test",(req,res) => {
+//     res.json({msg:"login works"})
+// })
 
 // $route  POST  api/users/register
 // @desc   返回的请求的json数据
@@ -34,7 +34,7 @@ router.post("/register",(req,res) => {
     User.findOne({email:req.body.email})
         .then((user) => {
             if(user){
-                return res.status(400).json({email:"邮箱已被注册"});
+                return res.status(400).json("邮箱已被注册!");
             }else{
                 const avatar = gravatar.url(req.body.email, {s: '200', r: 'pg', d: 'mm'});
 
@@ -42,7 +42,8 @@ router.post("/register",(req,res) => {
                     name:req.body.name,
                     email:req.body.email,
                     avatar,
-                    password:req.body.password
+                    password:req.body.password,
+                    identity:req.body.identity
                 })
 
                 //加密
@@ -74,7 +75,7 @@ router.post("/login",(req,res) => {
     User.findOne({email})
         .then(user => {
             if(!user){
-                return res.status(404).json({email:"用户不存在"});
+                return res.status(404).json("用户不存在!");
             }
 
             //密码匹配
@@ -82,7 +83,12 @@ router.post("/login",(req,res) => {
                   .then(isMatch => {
                       if(isMatch){
                         //定义加密规则
-                        const rule = {id:user.id,name:user.name};
+                        const rule = {
+                            id:user.id,
+                            name:user.name,
+                            avatar:user.avatar,
+                            identity:user.identity
+                        };
 
                         // jwt.sign("规则","加密名字","过期时间","箭头函数");
                         jwt.sign(rule,keys.secretOrKey,{expiresIn:3600},(err,token) =>{
@@ -95,7 +101,7 @@ router.post("/login",(req,res) => {
                         });
                         // res.json({msg:"success"});
                       }else{
-                        return res.status(400).json({password:"密码错误"});
+                        return res.status(400).json("密码错误");
                     }
                   })    
         })
@@ -109,7 +115,8 @@ router.get("/current",passport.authenticate("jwt",{session:false}),(req,res) => 
     res.json({
         id:req.user.id,
         name:req.user.name,
-        email:req.user.email
+        email:req.user.email,
+        identity:req.user.identity
     });
 })
 
