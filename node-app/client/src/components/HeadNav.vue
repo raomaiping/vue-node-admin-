8a488a0a7e8b2344bd12c15c9834051ef1d97127
raomaiping <2582395486@qq.com>
2020-01-17 18:00:46 +0800

    设置下拉菜单

--- node-app/client/src/components/HeadNav.vue
+++ node-app/client/src/components/HeadNav.vue
@@ -6,23 +6,63 @@
         <span class="title">前端小菜鸟吖</span>
       </el-col>
       <el-col :span="6" class="user">
-          <div class="userinfo">
-              <img src="//www.gravatar.com/avatar/93f9f4133dbc8c2b6cc9f3be5414304c?s=200&r=pg&d=mm" alt="" class="avatar">
-              <div class="welcome">
-                  <p class="name comename">欢迎</p>
-                  <p class="name avatarname">前端小菜鸟吖</p>
-              </div>
-              <span class="username">
-                  <!-- 下拉箭头 -->
-              </span>
+        <div class="userinfo">
+          <img :src="user.avatar" alt="" class="avatar">
+          <div class="welcome">
+            <p class="name comename">欢迎</p>
+            <p class="name avatarname">{{user.name}}</p>
           </div>
+          <span class="username">
+            <!-- 下拉箭头 -->
+            <el-dropdown trigger="click" @command="setDialogInfo">
+              <span class="el-dropdown-link">
+                <i class="el-icon-caret-bottom el-icon--right"></i>
+              </span>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item command="info">个人信息</el-dropdown-item>
+                <el-dropdown-item command="logout">退出</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+          </span>
+        </div>
       </el-col>
     </el-row>
   </header>
 </template>
 <script>
 export default {
-  name: "head-nav"
+  name: "head-nav",
+  computed:{
+      user(){
+          return this.$store.getters.user;
+      }
+  },
+  methods:{
+      setDialogInfo(cmdItem) {
+        //   console.log(cmdItem);
+        switch(cmdItem){
+            case "info":
+                this.showInfoList();
+                break;
+            case "logout":
+                this.logout();
+                break;
+        }
+          
+      },
+      showInfoList(){
+          console.log('个人信息');
+          
+      },
+      logout(){
+         //清除token
+         localStorage.removeItem('eleToken');
+         //设置vuex store
+         this.$store.dispatch('clearCurrentState');
+          //跳转
+          this.$router.push('/login');
+      }
+  }
 };
 </script>
 
