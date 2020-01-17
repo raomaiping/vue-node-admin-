edd7ce04e177a46012b556c6770dd9829ab08e87
raomaiping <2582395486@qq.com>
2020-01-17 22:07:47 +0800

    设置左侧导航栏

--- node-app/client/src/views/index.vue
+++ node-app/client/src/views/index.vue
@@ -1,17 +1,20 @@
 <template>
     <div class="index">
        <HeadNav></HeadNav>
+       <LeftMenu></LeftMenu>
        <router-view></router-view>
     </div>
 </template>
 
 
 <script>
-import HeadNav from '../components/HeadNav'
+import HeadNav from '../components/HeadNav';
+import LeftMenu from '../components/LeftMenu';
 export default {
     name:"index",
     components:{
-        HeadNav
+        HeadNav,
+        LeftMenu
     }
 };
 </script>
